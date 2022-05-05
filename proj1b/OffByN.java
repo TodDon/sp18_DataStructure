f54e8c9f4aa57163217cb55dca09c36681359e46
TodDon <82206615+TodDon@users.noreply.github.com>
2022-05-05 15:55:15 +0800

    Update OffByN.java

--- proj1b/OffByN.java
+++ proj1b/OffByN.java
@@ -10,20 +10,6 @@ public class OffByN implements CharacterComparator {
 
     @Override
     public boolean equalChars(char x, char y) {
-        int fx = x <= 'z' && x >= 'a' ? 0 : 1;
-        int fy = y <= 'z' && y >= 'a' ? 0 : 1;
-
-        if (fx == 1) {
-            fx = x <= 'Z' && x >= 'A' ? 1 : 2;
-        }
-
-        if (fy == 1) {
-            fy = y <= 'Z' && y >= 'A' ? 1 : 2;
-        }
-
-        if (fx == fy) {
-            return Math.abs(x - y) == offN;
-        }
-        return false;
+        return Math.abs(x - y) == offN;
     }
 }
